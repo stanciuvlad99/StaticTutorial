f3635bf9c62f66aae5bff20dcb67a2d780f9d32e
stanciuvlad99 <109428159+stanciuvlad99@users.noreply.github.com>
2023-05-16 20:19:31 +0300

    Update

--- src/ro/mycode/Main.java
+++ src/ro/mycode/Main.java
@@ -2,18 +2,40 @@ package ro.mycode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(metodaStatica());
+        System.out.println("Interiorul metodei statice " + metodaStatica());
+        System.out.println("Interiorul blocului static " + numar);
+        System.out.println("Interiorul clasei statice " + Static.clasaStatica());
     }
 
-    //Aceata este o varibila declarata statica
-    static int variabilaStatica =10;
+    // Variabila si metoda statica:
+    // Atunci cand o variabila sau o metoda este declarata ca statica, aceasta apartine clasei in sine, nu unei instante specifice a clasei.
+    // Variabilele statice sunt impartasite de toate instantele clasei si pot fi accesate fara a crea o instanță noua.
+    // Metodele statice pot fi apelate direct prin numele clasei, fara a crea o instanta.
+    static int variabilaStatica = 10;
 
-    //Aceasta este o variabila declarata nonstatica
-    int variabilaNestatica=10;
-
-    //Doar variabilele statice pot fi apelate in metodele statice "metodaStatica"
-    //Metodele si variabile statice pot fi folosit doar in clasa din care fac parte
-    public static int metodaStatica(){
+    public static int metodaStatica() {
         return variabilaStatica;
     }
+
+    // Bloc static:
+    // Un bloc static este un bloc de cod intr-o clasa care este executat o singura data atunci cand clasa este incarcata in memorie.
+    // Aceasta poate fi utilizata pentru a initializa variabile statice sau pentru a realiza alte operatii care trebuie efectuate o singura data.
+
+    static int numar;
+
+    static {
+        numar = 20;
+    }
+
+    // Clasa statica inglobata:
+    // In Java, o clasă poate fi definita in interiorul altei clase, iar daca este marcată ca statică, devine o clasa statică inglobata.
+    // O clasă statica inglobata poate fi accesata fara a crea o instanta a clasei exterioare si are restrictii asupra accesului la membrii clasei exterioare.
+
+    static class Static {
+        public static int clasaStatica() {
+            int numar = 30;
+            return numar;
+        }
+    }
+
 }

e1d95b917f450030c4c396755b4afe4747702260
stanciuvlad99 <109428159+stanciuvlad99@users.noreply.github.com>
2023-05-16 20:33:17 +0300

    Update

--- src/ro/mycode/Main.java
+++ src/ro/mycode/Main.java
@@ -2,19 +2,22 @@ package ro.mycode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Interiorul metodei statice " + metodaStatica());
+        System.out.println("Interiorul metodei statice " + MetodaStatica.metodaStatica());
         System.out.println("Interiorul blocului static " + numar);
-        System.out.println("Interiorul clasei statice " + Static.clasaStatica());
+        System.out.println("Interiorul clasei statice " + StaticClass.Static.functieStatica());
     }
 
     // Variabila si metoda statica:
     // Atunci cand o variabila sau o metoda este declarata ca statica, aceasta apartine clasei in sine, nu unei instante specifice a clasei.
     // Variabilele statice sunt impartasite de toate instantele clasei si pot fi accesate fara a crea o instanță noua.
     // Metodele statice pot fi apelate direct prin numele clasei, fara a crea o instanta.
-    static int variabilaStatica = 10;
 
-    public static int metodaStatica() {
-        return variabilaStatica;
+    public class MetodaStatica {
+        static int variabilaStatica = 10;
+
+        public static int metodaStatica() {
+            return variabilaStatica;
+        }
     }
 
     // Bloc static:
@@ -31,10 +34,14 @@ public class Main {
     // In Java, o clasă poate fi definita in interiorul altei clase, iar daca este marcată ca statică, devine o clasa statică inglobata.
     // O clasă statica inglobata poate fi accesata fara a crea o instanta a clasei exterioare si are restrictii asupra accesului la membrii clasei exterioare.
 
-    static class Static {
-        public static int clasaStatica() {
-            int numar = 30;
-            return numar;
+    private static int variabilaInafra=30;
+    public static class StaticClass {
+
+        static class Static {
+            static int variabilaInauntru = 40;
+            public static int functieStatica() {
+                return variabilaInafra+variabilaInauntru;
+            }
         }
     }
 
